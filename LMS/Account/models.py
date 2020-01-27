0cfe7f764511060060cf10e352c6393e6062a1bd
niraj-khatiwada <nirazzz.nk@gmail.com>
2020-01-27 17:20:14 +0545

    'ajnsjas'

--- LMS/Account/models.py
+++ LMS/Account/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from django.core import validators
+
 from django.core.exceptions import ValidationError
 from django.contrib.auth.models import  AbstractBaseUser, BaseUserManager
 
--- LMS/Account/validators.py
+++ LMS/Account/validators.py
@@ -1,8 +1,9 @@
 from django.core.exceptions import ValidationError
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext_lazy 
 import string
 
-def validate_username(value):
-    if not (string.ascii_letters+string.digits) and '_' and '.' and '-' in value:
-        raise ValidationError(_('Username can have "numbers letters _ - ." only')) 
-    return value
+def validate_username(username):
+    for i in username:
+        if not i in string.ascii_letters and not i in string.digits and i != '-' and i != '_' and i != '.':
+            raise ValidationError(ugettext_lazy('Username can have "Numbers A-Z a-z _ - ." only'))
+    return username
