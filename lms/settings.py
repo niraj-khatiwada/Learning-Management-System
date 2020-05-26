baa19290471db337eaa2ab384dd4a31ee897556b
niraj-khatiwada <nirazzz.nk@gmail.com>
2020-05-26 13:29:31 +0545

    Deploying

--- lms/settings.py
+++ lms/settings.py
@@ -24,7 +24,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
