9401ac0c0cb3d3ea455a93f87fb04580be2c2ff6
niraj-khatiwada <nirazzz.nk@gmail.com>
2020-05-26 13:33:38 +0545

    Deploying

--- lms/settings.py
+++ lms/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMilddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
